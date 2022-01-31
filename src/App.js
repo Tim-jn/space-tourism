d67fd9341a2ae914bf2c0c443fc2054537ff7c08
Tim <Tim280696@hotmail.com>
2022-01-31 10:16:41 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css'
 import Home from './Pages/Home/Home'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Destination from './Pages/Destination/Destination'
+import Crew from './Pages/Crew/Crew'
+import Technology from './Pages/Technology/Technology'
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/destination" element={<Destination />} />
+        <Route exact path="/crew" element={<Crew />} />
+        <Route exact path="/technology" element={<Technology />} />
+      </Routes>
     </Router>
   )
 }
