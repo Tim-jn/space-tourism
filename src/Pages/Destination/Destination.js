564a079c7f1cf3705e8fa98ac0abb519318c181f
Tim <Tim280696@hotmail.com>
2022-02-08 15:12:42 +0100

    Added data from json

--- src/Pages/Destination/Destination.js
+++ src/Pages/Destination/Destination.js
@@ -6,8 +6,13 @@ import moon from './image-moon.png'
 import mars from './image-mars.png'
 import europa from './image-europa.png'
 import titan from './image-titan.png'
+import datas from '../../Data/data.json'
 
 export default function Destination() {
+  const planetData = datas.destinations.map((data) => {
+    return data
+  })
+
   const [moonDisplay, setMoonDisplay] = useState(true)
   const [marsDisplay, setMarsDisplay] = useState(false)
   const [europaDisplay, setEuropaDisplay] = useState(false)
@@ -82,34 +87,34 @@ export default function Destination() {
         {moonDisplay ? (
           <Planet
             planetImage={moon}
-            planetName="MOON"
-            planetDesc="See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites."
-            planetDist="384,400 KM"
-            travelTime="3 DAYS"
+            planetName={planetData[0].name}
+            planetDesc={planetData[0].description}
+            planetDist={planetData[0].distance}
+            travelTime={planetData[0].travel}
           />
         ) : marsDisplay ? (
           <Planet
             planetImage={mars}
-            planetName="MARS"
-            planetDesc="Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!"
-            planetDist="225 MIL. KM"
-            travelTime="9 MONTHS"
+            planetName={planetData[1].name}
+            planetDesc={planetData[1].description}
+            planetDist={planetData[1].distance}
+            travelTime={planetData[1].travel}
           />
         ) : europaDisplay ? (
           <Planet
             planetImage={europa}
-            planetName="EUROPA"
-            planetDesc="The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin."
-            planetDist="628 MIL. KM"
-            travelTime="3 YEARS"
+            planetName={planetData[2].name}
+            planetDesc={planetData[2].description}
+            planetDist={planetData[2].distance}
+            travelTime={planetData[2].travel}
           />
         ) : titanDisplay ? (
           <Planet
             planetImage={titan}
-            planetName="TITAN"
-            planetDesc="The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn."
-            planetDist="1.6 BIL KM"
-            travelTime="7 YEARS"
+            planetName={planetData[3].name}
+            planetDesc={planetData[3].description}
+            planetDist={planetData[3].distance}
+            travelTime={planetData[3].travel}
           />
         ) : (
           ''
